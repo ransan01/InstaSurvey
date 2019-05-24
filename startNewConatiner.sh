79c81da725b4779cf077807c6585fc2fca38ebc8
Sanjay Rana <RanSan01@CSGICORP.COM>
2019-05-24 13:34:49 +0530

    scripts added and Readme changes.

--- startNewConatiner.sh
+++ startNewConatiner.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 docker run -i -t -d -p 4200:4200 --name devenv --hostname wolverine wolverine
 
 docker exec -i -t -d devenv sh -c "cd /home/InstantSurvey/server && node_modules/.bin/babel-node app.ts"
