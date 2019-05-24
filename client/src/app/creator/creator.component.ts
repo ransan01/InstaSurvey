ef439633bbc492cae953fedf50394dd9511d7193
Phalgun Vaddepalli <VadPha01@CSGICORP.COM>
2019-05-24 14:23:42 +0530

    changed default question

--- client/src/app/creator/creator.component.ts
+++ client/src/app/creator/creator.component.ts
@@ -19,76 +19,11 @@ Survey.StylesManager.applyTheme('default');
 
 
 export class CreatorComponent implements OnInit {
-  json = {
-    completedHtml: '<h3>Thank you for your feedback.</h3> <h5>Your thoughts and ideas will help us to create a great product!</h5>',
-    completedHtmlOnCondition: [
-      {
-        expression: '{nps_score} > 8',
-        html: '<h3>Thank you for your feedback.</h3> <h5>We glad that you love our product.' +
-        ' Your ideas and suggestions will help us to make our product even better!</h5>'
-      }, {
-        expression: '{nps_score} < 7',
-        html: '<h3>Thank you for your feedback.</h3> <h5> We are glad that you share with us your ideas.' +
-        'We highly value all suggestions from our customers. We do our best to improve the product and reach your expectation.</h5>\n'
-      }
-    ],
-    pages: [
-      {
-        name: 'page1',
-        elements: [
-          {
-            type: 'rating',
-            name: 'nps_score',
-            title: 'On a scale of zero to ten, how likely are you to recommend our product to a friend or colleague?',
-            isRequired: true,
-            rateMin: 0,
-            rateMax: 10,
-            minRateDescription: '(Most unlikely)',
-            maxRateDescription: '(Most likely)'
-          }, {
-            type: 'checkbox',
-            name: 'promoter_features',
-            visibleIf: '{nps_score} >= 9',
-            title: 'What features do you value the most?',
-            isRequired: true,
-            validators: [
-              {
-                type: 'answercount',
-                text: 'Please select two features maximum.',
-                maxCount: 2
-              }
-            ],
-            hasOther: true,
-            choices: [
-              'Performance', 'Stability', 'User Interface', 'Complete Functionality'
-            ],
-            otherText: 'Other feature:',
-            colCount: 2
-          }, {
-            type: 'comment',
-            name: 'passive_experience',
-            visibleIf: '{nps_score} > 6  and {nps_score} < 9',
-            title: 'What is the primary reason for your score?'
-          }, {
-            type: 'comment',
-            name: 'disappointed_experience',
-            visibleIf: '{nps_score} notempty',
-            title: 'What do you miss and what was disappointing in your experience with us?'
-          }
-        ]
-      }
-    ],
-    showQuestionNumbers: 'off'
-  };
   private creator;
+  private json;
   constructor(private  httpClient: HttpClient) { }
 
   ngOnInit() {
-    /*let survey = new Survey.Model(this.json);
-    survey.onComplete.add((result) => {
-      });
-
-    Survey.SurveyNG.render('surveyElement', { model: survey });*/
     Survey.StylesManager
       .applyTheme('bootstrap');
 
@@ -120,8 +55,63 @@ export class CreatorComponent implements OnInit {
           }
         });
     };
-
-    this.creator.text = '{ pages: [{ name:\'page1\', questions: [{ type: \'text\', name:\"Test question\"}]}]}';
+    const defaultQuestion = {
+      pages: [
+        {
+          name: 'page1',
+          elements: [
+            {
+              type: 'radiogroup',
+              name: 'question1',
+              title: 'Who do you think should win CSG Hackathon 2019',
+              choices: [
+                'LectoLectro',
+                'Lone Coder',
+                'Night Watchers',
+                'No One',
+                'ATS-6733',
+                'Not so Fantastic Four',
+                'Unhandled Exceptions',
+                'D Bug Hunters',
+                'MKS-4',
+                'F1-Fear One',
+                'SIMBA',
+                'SVeeter',
+                'Unfired',
+                'Ascendon Tiger Team',
+                'Infinite-3',
+                'CSG Titans',
+                'Team Jhappi',
+                'OperationHack',
+                'Anonymous Hackers',
+                'Formula-F2',
+                'WhattsApp Us',
+                'HackOut',
+                'CSGOne',
+                'Desi Boyz',
+                'Formula Five',
+                'Thugs of CSG',
+                'PackOf3',
+                'ACSR VISION',
+                'Avengers 5.0',
+                'CodeCrackers',
+                'Techie Tribe',
+                'Anuradha',
+                'Ravali Madabhushanam',
+                'The Mavericks',
+                'Bro-grammers',
+                'Powerpuff Girls',
+                'beep',
+                'M2 ',
+                'DbDudez',
+                'Sirius'
+              ]
+            }
+          ]
+        }
+      ]
+    };
+    this.creator.text = JSON.stringify(defaultQuestion);
     this.loadSurvey();
   }
 
@@ -152,7 +142,6 @@ export class CreatorComponent implements OnInit {
       minRateDescription: '(Most unlikely)',
       maxRateDescription: '(Most likely)'
     };
-    console.log(this.json.pages[0].elements);
     this.json.pages[0].elements.push(newQuestion);
     const survey = new Survey.Model(this.json);
     Survey.SurveyNG.render('surveyElement', { model: survey });
